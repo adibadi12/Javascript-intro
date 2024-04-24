c256113657f0e53e2cb41b17e2db403d64dcbf8d
adibadi12 <adrian.mathiassen@gmail.com>
2024-04-24 10:44:27 +0200

    Showcase linking and variables

---
+++ main.js
@@ -0,0 +1,21 @@
+// Definer en variabel
+let greeting
+//gi den en verdi
+ greeting = "god morgen"
+// Gjøre noe med variabelen
+ console.log(greeting)
+
+ //omdefinere verdien til variablene (gi den en ny verdi)
+ greeting = "en ny verdi"
+ console.log(greeting)
+
+ // Definere konstanter
+ const constantvariabel = "i am a constant"
+
+ //neste linje crasher programmet
+ // constantvariabel = "this does not work"
+
+ /**
+  * flere linjer med
+  * kommentarer
+  */
