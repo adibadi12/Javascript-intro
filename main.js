4d3893f4f3dc0dd34038964c3681ae1daa1713a7
adibadi12 <adrian.mathiassen@gmail.com>
2024-04-24 12:03:13 +0200

    fix theme switcher

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ let greeting
  // Definere konstanter
  const constantvariabel = "i am a constant"
 
+ let changeablevariable = "testing"
+
  //neste linje crasher programmet
  // constantvariabel = "this does not work"
 
@@ -19,3 +21,48 @@ let greeting
   * flere linjer med
   * kommentarer
   */
+ let notDefined
+ console.log(notDefined)
+
+ let notYetDefined = null
+ console.log (notYetDefined)
+
+ //komplekse typer
+ const number = [ 1, 2, 3, 4]
+ console.log(numbers)
+
+ // Samlinger med informasjon (objects)
+ Objekter
+ const person = {
+    email: "adrian.mathiassen@gmail.com",
+    name: "Adrian mathiassen",
+    age: 17
+}
+
+
+console.log(person.name)
+
+/**
+ * ==========================
+ * change theme functioanlity
+ * ==========================
+ */
+
+//først finne knappen <button> elementet
+let buttonElement = document.getElementById("theme-changer")
+
+function changeTheme() {
+    buttonElement.textContent="it worked!!"
+    //først finne body elementet
+    const body = document.getElementById("body")
+
+    //så finn ut hva klasse navnet er
+    const currentTheme =body.className
+    console.log(currentTheme,)
+
+    //viss lyst tema skift til mørkt
+
+    //viss mørkt skift til lys
+}
+
+buttonElement.addEventListener("click", changeTheme)
